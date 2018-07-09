78c91a07c641315309aef610ab53cec4659ae4c5
shockk73 <31765979+shockk73@users.noreply.github.com>
2018-07-09 17:44:04 +0300

    Add files via upload

---
+++ s.py
@@ -0,0 +1,16 @@
+import socket
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+sock = socket.socket()
+sock.bind(('195.54.163.190', 8888))
+sock.listen(1)
+
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
+    driver.get(data.decode());
+    search_box = driver.find_element_by_tag_name('body').text
+    conn.send(search_box.encode())
+    conn.close()
